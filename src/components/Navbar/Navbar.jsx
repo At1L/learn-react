ed9f94d719590f2e2bdba71703f5e6dbeab7c29d
Kozlov Arseny <jetpilotat1il@gmail.com>
2023-11-22 22:10:20 +0300

    change to navlink and navbar elements style change

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,26 @@
-import classes from './Navbar.module.css'
+import { NavLink } from 'react-router-dom';
+import s from './Navbar.module.css'
 const Navbar = () => {
     return (
-    <nav className={classes.nav}>
-        <div className={classes.item}>
-            <a href='/profile'>Profile</a>
+    <nav className={s.nav}>
+        <div className={s.item}>
+            <NavLink to='/profile' className={({isActive}) => isActive ? s.activeLink : undefined} >Profile</NavLink>
         </div>
 
-        <div className={classes.item} href='/news'>
-            <a href='/news'>News</a>
+        <div className={s.item}>
+            <NavLink to='/news' className={({isActive}) => isActive ? s.activeLink : undefined}>News</NavLink>
         </div>
 
-        <div className={classes.item} href='/dialogs'>
-            <a href='/dialogs'>Messages</a>
+        <div className={s.item}>
+            <NavLink to='/dialogs' className={({isActive}) => isActive ? s.activeLink : undefined}>Messages</NavLink>
         </div>
 
-        <div className={classes.item}>
-            <a href='/music'>Music</a>
+        <div className={s.item}>
+            <NavLink to='/music' className={({isActive}) => isActive ? s.activeLink : undefined}>Music</NavLink>
         </div>
 
-        <div className={classes.item}>
-            <a href='/settings'>Settings</a>
+        <div className={s.item}>
+            <NavLink to='/settings' className={({isActive}) => isActive ? s.activeLink : undefined} >Settings</NavLink>
         </div>
     </nav>
     );
