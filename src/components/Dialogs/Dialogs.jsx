f31501d57282c845bcf2eaa51b6244f59c9cc4bf
Kozlov Arseny <jetpilotat1il@gmail.com>
2023-11-22 22:37:21 +0300

    messages page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,8 +1,20 @@
-import style from './Dialogs.module.css'
+import s from './Dialogs.module.css'
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Osipov Denis
+                </div>
+                <div className={s.dialog}>
+                    Lysikov Daniil
+                </div>
+            </div>
+            <div className={s.message}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How react going</div>
+                <div className={s.message}>stayin` Alive</div>
+            </div>
         </div>
     );
 }
