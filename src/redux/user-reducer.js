b1df7a1a756835185f5a6ebc76a45d65f2dc740b
Kozlov Arseny <jetpilotat1il@gmail.com>
2024-02-28 21:04:04 +0300

    add axios and users page now take data from server

--- src/redux/user-reducer.js
+++ src/redux/user-reducer.js
@@ -2,14 +2,7 @@ const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS'
 let initState = {
-    users: [
-        { id : 1, photoURL: 'https://sun9-41.userapi.com/impg/LQIHsOvjkvMri2w-doq1PvoIQRUrjihsXgHk3Q/tLldmPegd_Y.jpg?size=181x214&quality=96&sign=0a067f8b60136eb2d439cb2a612e4fee&type=album',
-         followed: true, fullName : 'Denis', status: 'krutoi chel', location:{city:'Soyuznaya 144', country:'Bragino'}},
-        { id : 2, photoURL: 'https://sun9-65.userapi.com/impg/rHyfYgXOtqn0v7QBWycyBmCAcLSfYPGalr5E3Q/9AZHRAJIMHE.jpg?size=833x1080&quality=95&sign=74ab3c3c21b1f59e941c2bc223a92c65&type=album',
-         followed: true, fullName : 'Daniil', status: 'toje Denis', location:{city:'Soyuznaya 144', country:'Bragino'}},
-        { id : 3, photoURL: 'https://sun9-28.userapi.com/impg/H_Uu2ai-43Wf6QHeSZEgPYzWkZYzIrd_eo51uQ/IeB3bpT0iXM.jpg?size=828x728&quality=95&sign=e4c36ce90fae7949af1b926b31771ddb&type=album',
-         followed: false, fullName : 'Maria', status: 'Ferrari fan', location:{city:'Soyuznaya 144', country:'Bragino'}}
-      ]
+    users: []
 }
 const userReducer = (state = initState, action) => {
     switch(action.type){
