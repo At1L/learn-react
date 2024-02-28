2d94bc74a8a9db037692f9667f7cf9e65533da0b
Kozlov Arseny <jetpilotat1il@gmail.com>
2024-02-28 22:28:06 +0300

    user page is class now

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Users from "./Users";
+import Users from "./UsersClass";
 import { connect } from "react-redux";
 import { followAC, setUsersAC, unfollowAC } from "../../redux/user-reducer";
 
